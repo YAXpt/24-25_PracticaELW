a0bfe47501401b633b236c071e26e8678f80d029
YAXpt <aylachic@gmail.com>
2024-11-16 19:32:52 +0100

    Angular1

---
+++ Practica/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+    password: new FormControl('', { validators: [Validators.required] })
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    alert('Form submitted');
+  }
+
+}
