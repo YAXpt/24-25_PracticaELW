7a743f0c4b6cf91c165bd2df5f096b02b49511a8
YAXpt <aylachic@gmail.com>
2024-12-17 17:56:43 +0100

    comit

---
+++ Practica/src/app/user-area/user-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-area.component.html',
+  styleUrl: './user-area.component.css'
+})
+export class UserAreaComponent {
+
+}
